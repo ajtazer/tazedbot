d184690d4094b81c9ef4ba1191a753a37fd5daca
Anuj Rawat <37796611+ajtazer@users.noreply.github.com>
2020-08-16 20:35:58 +0530

    Basic Telegram Bot Files

---
+++ telegrambot.py
@@ -0,0 +1,33 @@
+import telebot
+from flask import Flask, request
+import os
+
+TOKEN = "693102475:AAGy58dc9eRPwVMhUEwu3G0OF-zyJm1sZlo"
+bot = telebot.TeleBot(token=TOKEN)
+server = Flask(__name__)
+
+def findat(msg):
+    # from a list of texts, it finds the one with the '@' sign
+    for i in msg:
+        if '@' in i:
+            return i
+
+@bot.message_handler(commands=['start']) # welcome message handler
+def send_welcome(message):
+    bot.reply_to(message, '(placeholder text)')
+
+@bot.message_handler(commands=['help']) # help message handler
+def send_welcome(message):
+    bot.reply_to(message, 'ALPHA = FEATURES MAY NOT WORK')
+
+@bot.message_handler(func=lambda msg: msg.text is not None and '@' in msg.text)
+# lambda function finds messages with the '@' sign in them
+# in case msg.text doesn't exist, the handler doesn't process it
+def at_converter(message):
+    texts = message.text.split()
+    at_text = findat(texts)
+    if at_text == '@': # in case it's just the '@', skip
+        pass
+    else:
+        insta_link = "https://instagram.com/{}".format(at_text[1:])
+        bot.reply_to(message, insta_link)
